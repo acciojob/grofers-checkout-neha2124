bf792a1b1881f78e66f94f29dd7c2ee9d1cd42e4
neha2124 <68601648+neha2124@users.noreply.github.com>
2023-02-23 00:36:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
-
-const getSum = () => {
-//Add your code here
-	const table = document.querySelector("table")
+const table = document.querySelector("table")
 	const price = document.querySelectorAll('.price')
 	const td = document.createElement("tr")
+const getSum = () => {
+//Add your code here
+	
 	let tp = Array.from(price);
 	let sum = 0;
 	tp.forEach((item)=>{
