ccf04fbc7294174c56b13c175b89d67889d7d046
neha2124 <68601648+neha2124@users.noreply.github.com>
2023-02-23 11:19:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,16 +4,17 @@ document.body.appendChild(getSumBtn);
 const table = document.querySelector("table")
 	const price = document.querySelectorAll('.price')
 	const td = document.createElement("tr")
+    let tp = Array.from(price);
+	let sum = 0;
 const getSum = () => {
 //Add your code here
 	
-	let tp = Array.from(price);
-	let sum = 0;
+	
 	tp.forEach((item)=>{
          let a =parseInt(item.innerText)
 		sum += a
 	})
-	td.innerHTML = `<td>total value</td>
+	td.innerHTML = `<td>Total price</td>
  <td>${sum}</td>`
 	table.append(td)
 	// console.log(sum)
